242563942c7997a1976237d1990a454d90ebe8f7
RoenWesily <1078559360@qq.com>
2026-08-17 21:37:34 +0800

    配置数学公式渲染支持

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,12 +1,16 @@
-import { remark } from 'remark';
-import remarkGfm from 'remark-gfm';
-import remarkHtml from 'remark-html';
+import { remark } from 'remark'
+import html from 'remark-html'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+import rehypeStringify from 'rehype-stringify'
+import remarkRehype from 'remark-rehype'
 
-export async function markdownToHtml(markdown: string): Promise<string> {
+export async function markdownToHtml(markdown: string) {
   const result = await remark()
-    .use(remarkGfm)
-    .use(remarkHtml)
-    .process(markdown);
-
-  return result.toString();
+    .use(remarkMath)              // 解析 LaTeX 公式
+    .use(remarkRehype)            // 转换为 HTML 语法树
+    .use(rehypeKatex)             // 渲染 LaTeX 公式为 HTML
+    .use(rehypeStringify)         // 转换为 HTML 字符串
+    .process(markdown)
+  return result.toString()
 }
